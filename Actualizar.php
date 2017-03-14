ce18f1ca2e750dd9d1bdd182050b0767cac8f851
Alma <Alma>
2017-03-13 22:01:29 -0600

    cambios

--- Actualizar.php
+++ Actualizar.php
@@ -17,7 +17,7 @@ $reg=mysqli_query($conn,$sql);
 if($reg){
 echo "Nombre:".$nom." Dirección:".$dir." RFC:".$rfc." Tel:".$tel." Clasificación:".$clasif." Foto:".$foto." Latitud:".$lat." Longitud:".$longit;
 }else{
-echo "Error al registrar los datos:".mysqli_error();	
+echo "Error al registrar los datos:".mysqli_error().$nom.$dir.$rfc.$tel.$clasif.$foto.$lat.$longit;	
 }
 
 mysqli_close($conn);
